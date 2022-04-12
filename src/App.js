59c9bef511e6a971954b56b359f4f4036acf49f4
Shea Cronin <shealcronin@gmail.com>
2022-04-12 13:04:47 -0400

    Add Header, Login, and SignUp components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import './App.css';
+import Header from './components/Header';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
-    return <div>hello world</div>;
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    return (
+        <Router>
+            <Header user={user} setUser={setUser} />
+            <main>
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            isLoading ? (
+                                <div>Loading...</div>
+                            ) : (
+                                <div>hello world</div>
+                            )
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/login"
+                        element={<Login setUser={setUser} />}
+                    />
+                    <Route exact path="/signup" element={<SignUp />} />
+                </Routes>
+            </main>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import '../styles/Header.css';
+import { Link } from 'react-router-dom';
+
+function Header({ user, setUser }) {
+    function logoutUser() {
+        setUser(null);
+        localStorage.clear();
+    }
+
+    return (
+        <header>
+            <Link to="/" className="site-title">
+                <h1>Bloggit</h1>
+                <div className="site-desc">Author</div>
+            </Link>
+            <nav>
+                <ul>
+                    {user === null ? (
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/signup">Sign Up</Link>
+                            </li>
+                        </>
+                    ) : (
+                        <li>
+                            <button onClick={logoutUser}>Logout</button>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
