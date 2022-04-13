c81346925f673da584016a95e2a99e570168154a
Shea Cronin <shealcronin@gmail.com>
2022-04-13 12:26:28 -0400

    Add ability to delete posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,11 @@
 import '../styles/Post.css';
 import { formatRelative, parseISO } from 'date-fns';
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Post({ post, setPosts, setPost, isSinglePost }) {
     let navigate = useNavigate();
+    const [isPromptingToDelete, setIsPromptingToDelete] = useState(false);
 
     async function togglePostPublished() {
         const res = await fetch(
@@ -43,6 +45,31 @@ function Post({ post, setPosts, setPost, isSinglePost }) {
         navigate(`/edit-post/${post._id}`);
     }
 
+    function promptToDelete() {
+        setIsPromptingToDelete(true);
+    }
+
+    function stopDeletePrompt() {
+        setIsPromptingToDelete(false);
+    }
+
+    async function deletePost() {
+        await fetch(`http://localhost:3001/posts/${post._id}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('token'),
+            },
+        });
+
+        if (isSinglePost) {
+            navigate('/');
+        } else {
+            setPosts((prevPosts) =>
+                prevPosts.filter((aPost) => aPost._id !== post._id)
+            );
+        }
+    }
+
     return (
         <article className="post">
             <div className="post-info">
@@ -52,17 +79,32 @@ function Post({ post, setPosts, setPost, isSinglePost }) {
                 </p>
             </div>
             <p>{post.content}</p>
-            <div className="post-publish">
+            <div className="post-buttons">
                 <button className="edit-button" onClick={handleEditPostClick}>
                     Edit
+                </button>
+                <button className="delete-button" onClick={promptToDelete}>
+                    Delete
                 </button>
                 <p className="post-status">
                     {post.isPublished ? 'Published ✅' : 'Draft Post 📝'}
                 </p>
-                <button onClick={togglePostPublished}>
+                <button
+                    onClick={togglePostPublished}
+                    className="publish-button"
+                >
                     {post.isPublished ? 'Unpublish' : 'Publish'}
                 </button>
             </div>
+            {isPromptingToDelete && (
+                <div className="delete-prompt">
+                    Are you sure you want to delete this post?{' '}
+                    <button className="delete-button" onClick={deletePost}>
+                        Yes
+                    </button>{' '}
+                    <button onClick={stopDeletePrompt}>No</button>
+                </div>
+            )}
             {!isSinglePost && (
                 <div className="view-comments">
                     <Link to={`/posts/${post._id}`}>View Comments</Link>
